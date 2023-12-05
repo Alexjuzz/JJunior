d4e95074c1406be18598f7e3c04d4beef6416ab6
Alex <juzzle@mail.ru>
2023-12-05 17:34:49 +0300

    tst1

--- lesson1/src/main/java/ru/geekbrains/lesson1/task2/Cart.java
+++ lesson1/src/main/java/ru/geekbrains/lesson1/task2/Cart.java
@@ -2,6 +2,7 @@ package ru.geekbrains.lesson1.task2;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Cart<T extends Food> {
@@ -21,10 +22,10 @@ public class Cart<T extends Food> {
 
     /**
      * Создание нового экземпляра корзины
+     *
      * @param market принадлежность к магазину
      */
-    public Cart(Class<T> clazz, UMarket market)
-    {
+    public Cart(Class<T> clazz, UMarket market) {
         this.clazz = clazz;
         this.market = market;
         foodstuffs = new ArrayList<>();
@@ -35,57 +36,40 @@ public class Cart<T extends Food> {
     /**
      * Балансировка корзины
      */
-    public void cardBalancing()
-    {
-        boolean proteins = false;
-        boolean fats = false;
-        boolean carbohydrates = false;
+    public void cardBalancing() {
+        AtomicBoolean proteins = new AtomicBoolean(false);
+        AtomicBoolean fats = new AtomicBoolean(false);
+        AtomicBoolean carbohydrates = new AtomicBoolean(false);
+        foodstuffs.stream()
+                .filter(food -> !proteins.get() && food.getProteins())
+                .findFirst()
+                .ifPresent(food -> proteins.set(true));
 
-        for (var food : foodstuffs)
-        {
-            if (!proteins && food.getProteins())
-                proteins = true;
-            else
-            if (!fats && food.getFats())
-                fats = true;
-            else
-            if (!carbohydrates && food.getCarbohydrates())
-                carbohydrates = true;
-            if (proteins && fats && carbohydrates)
-                break;
-        }
+        foodstuffs.stream()
+                .filter(food -> !fats.get() && food.getFats())
+                .findFirst()
+                .ifPresent(food -> fats.set(true));
 
-        if (proteins && fats && carbohydrates)
-        {
-            System.out.println("Корзина уже сбалансирована по БЖУ.");
-            return;
-        }
+        foodstuffs.stream()
+                .filter(food -> !carbohydrates.get() && food.getCarbohydrates())
+                .findFirst()
+                .ifPresent(food -> carbohydrates.set(true));
 
-        for (var thing : market.getThings(clazz))
-        {
-            if (!proteins && thing.getProteins())
-            {
-                proteins = true;
-                foodstuffs.add(thing);
-            }
-            else if (!fats && thing.getFats())
-            {
-                fats = true;
-                foodstuffs.add(thing);
-            }
-            else if (!carbohydrates && thing.getCarbohydrates())
-            {
-                carbohydrates = true;
-                foodstuffs.add(thing);
-            }
-            if (proteins && fats && carbohydrates)
-                break;
-        }
+        market.getThings(clazz).stream()
+                        .filter(food -> (!proteins.get()) && food.getProteins()).findFirst().ifPresent(foodstuffs::add);
 
-        if (proteins && fats && carbohydrates)
+        market.getThings(clazz).stream()
+                .filter(food -> (!fats.get()) && food.getFats()).findFirst().ifPresent(foodstuffs::add);
+
+        market.getThings(clazz).stream()
+                .filter(food -> (!carbohydrates.get()) && food.getCarbohydrates()).findFirst().ifPresent(foodstuffs::add);
+
+
+        if (proteins.get() && fats.get() && carbohydrates.get())
             System.out.println("Корзина сбалансирована по БЖУ.");
         else
             System.out.println("Невозможно сбалансировать корзину по БЖУ.");
+
     }
 
 
@@ -96,8 +80,7 @@ public class Cart<T extends Food> {
     /**
      * Распечатать список продуктов в корзине
      */
-    public void printFoodstuffs()
-    {
+    public void printFoodstuffs() {
         AtomicInteger index = new AtomicInteger(1);
         foodstuffs.forEach(food -> System.out.printf("[%d] %s (Белки: %s Жиры: %s Углеводы: %s)\n",
                 index.getAndIncrement(), food.getName(),
