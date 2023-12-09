e7211ac037ac0b9115cbbf2ddc4b97c577997cd7
Alex <juzzle@mail.ru>
2023-12-09 13:21:36 +0300

    попытка сделать

--- lesson1/src/main/java/ru/geekbrains/task3/ReflectionTask1/task1.java
+++ lesson1/src/main/java/ru/geekbrains/task3/ReflectionTask1/task1.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.task3.ReflectionTask1;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 //Задача 1:
@@ -10,7 +11,7 @@ import java.lang.reflect.Method;
 //        Выведите на экран информацию о каждом объекте.
 //        Вызовите метод "makeSound()" у каждого объекта, если такой метод присутствует.
 public class task1 {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Cat cat = new Cat("Barsik", 10);
         Dog dog = new Dog("Kac", 5);
         Animal[] animals = new Animal[]{new Cat("BARSIK", 5), new Dog("Kac", 5)};
@@ -20,14 +21,15 @@ public class task1 {
         }
     }
 
-    static void inforMationAboutAnimals(Object obj) throws IllegalAccessException, NoSuchMethodException {
+    static void inforMationAboutAnimals(Object obj) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Class<?> clazz = obj.getClass();
         Field[] fields = clazz.getDeclaredFields();
         Method methods = clazz.getDeclaredMethod("toSing");
         for (Field f : fields
              ) {
             System.out.print(f.get(obj)+ " ");
+
         }
-        System.out.println(methods.getName());
+        methods.invoke(obj);
     }
 }
